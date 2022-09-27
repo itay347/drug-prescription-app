6c5adb7e5c0fe0afab24bc7d46e66efaafa37f97
Itay <itay647@gmail.com>
2022-09-27 15:07:16 +0300

    Add drug search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { Box, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [searchText, setSearchText] = useState<string>("");
+
+  useEffect(() => {
+    if (searchText && searchText.length > 0) {
+      console.log({searchText});
+    }
+  }, [searchText]);
+
   return (
     <div className="App">
-      <h1>Drug Prescription App</h1>
+      <Box sx={{ width: "50%" }}>
+        <h1>Drug Prescription App</h1>
+        <TextField
+          id="search-drug"
+          label="Search Drug"
+          variant="outlined"
+          fullWidth
+          type="text"
+          value={searchText}
+          onChange={(event) => setSearchText(event.target.value)}
+        />
+      </Box>
     </div>
   );
 }
