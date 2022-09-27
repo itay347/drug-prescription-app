6fd7e7d8627580bfc8e138e9a58dc98c8689d82c
Itay <itay647@gmail.com>
2022-09-27 15:54:30 +0300

    Load and show the search results in a list

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import { Box, TextField } from "@mui/material";
+import { Box, List, ListItemButton, ListItemText, TextField } from "@mui/material";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
 
+type Drug = {
+  code: number;
+  name: string;
+}
+
 function App() {
   const [searchText, setSearchText] = useState<string>("");
+  const [drugSearchResults, setDrugSearchResults] = useState<Drug[]>([]);
 
   useEffect(() => {
-    if (searchText && searchText.length > 0) {
-      console.log({searchText});
+    if (!(searchText && searchText.length > 0)) {
+      setDrugSearchResults([]);
+    } else {
+      (async () => {
+        try {
+          // TODO: use abort controller
+          let response = await axios.get(
+            "https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search",
+            {
+              params: { terms: searchText, ef: "RXCUIS" },
+            }
+          );
+          const data = response.data;
+          // NOTE: for the sake of this exercise I'm assuming there is only one 'strength' and taking the first code from RXCUIS
+          const drugs: Drug[] = data[1].map(
+            (name: string, index: number) =>
+              ({ code: data[2]["RXCUIS"][index][0], name: name } as Drug)
+          );
+          setDrugSearchResults(drugs);
+        } catch (e) {
+          // TODO: handle
+        }
+      })();
     }
   }, [searchText]);
 
@@ -24,6 +52,21 @@ function App() {
           value={searchText}
           onChange={(event) => setSearchText(event.target.value)}
         />
+        <List
+          sx={{
+            overflow: "auto",
+            height: "25vh",
+          }}
+        >
+          {drugSearchResults.map((drug) => {
+            // TODO: allow selection for adding drugs
+            return (
+              <ListItemButton key={drug.code}>
+                <ListItemText primary={drug.name} />
+              </ListItemButton>
+            );
+          })}
+        </List>
       </Box>
     </div>
   );
